156a7f308cf9b6ba33c66c1e6ae87a3bb3316cfd
devjeel <jeelhp02@gmail.com>
2019-04-10 22:31:42 -0400

    Automatic Functionality Added

--- bot.py
+++ bot.py
@@ -15,8 +15,9 @@ try:
     # Open the website
     driver.get('http://livestockcensus.gov.in/')
 
-    print("Logging in with username and password..\n")
+    print("Logging in ==> with username and password..\n")
 
+    # Username and Keys
     user_id_box = driver.find_element_by_id('userid')
     user_id_box.send_keys('')
 
@@ -24,66 +25,85 @@ try:
     pass_box = driver.find_element_by_id('password')
     # Send password
     pass_box.send_keys('')
-    # Find login button
-    login_button = driver.find_element_by_id('Submit')
-    # Click login
-    login_button.click()
 
-    print("Loding Village List....\n")
+    # Find login button
+    driver.find_element_by_id('Submit').click()
+
+    print("Loading Village List....\n")
 
     driver.get('http://livestockcensus.gov.in/schedule/scheduleDetailsVillageWise.action')
 
-    #Village number
+    # Village number
     village = input("Input Village No:")
-    #save = driver.find_element_by_tag_name('tr')[village+1].find_element_by_tag_name('td')[3].find_element_by_tag_name('a')
+    print("Loading Village No. ==> " + str(village)+"\n")
 
-    print("Loding Village No.:" + str(village)+"\n")
-
+    # Click on Respective link to that village
     driver.find_element_by_xpath('//*[@id="example"]/tbody/tr[' + str(village) + ']/td[4]').click()
 
-    print("Loding Entries one by one...")
+    print("Loading Entries one by one...")
 
     running = True
 
+    def alert_check():
+        try:
+            alert = driver.switch_to.alert.accept()
+        except:
+            time.sleep(1)
+            alert_check()
+
+    # This Function is to find first Draft Entry Link
     def find_the_link(i):
         try:
-            a_link = driver.find_element_by_xpath('//*[@id="example"]/tbody/tr['+ str(i) +']/td[8]/a').click()
-        except:
+            a_link = driver.find_element_by_xpath('//*[@id="example"]/tbody/tr[' + str(i) + ']/td[8]/a').click()
+        except Exception:
             find_the_link(i=i+1)
 
+    def find_the_data_class(j):
+        try:
+            driver.find_element_by_xpath('/html/body/div/div[3]/div/div/div/form/div[1]/div/a[' + str(j) + ']').click()
+        except Exception:
+            if j >= 0:
+                find_the_data_class(j=j-1)
+
+    # Infinite Loop That runs for Every entry
     while running:
-        #print(driver.find_element_by_xpath('//*[@id="example"]/tbody/tr[1]/td[8]/a'))
-        #'//*[@id="example"]/tbody/tr[6]/td[8]'
-        #all_links = driver.find_element_by_xpath('/html/body/div/div[3]/div/div/div/form/div[1]/div')
-        #for link in all_links:
-            #link.get_attribute('href').click()
-            #time.sleep(25)
         find_the_link(i=1)
 
-        keyboard = input('Entry Values: Enter T for True / F for False')
+        # inside tabareadata find the href text
+        find_the_data_class(j=4)
 
-        if(keyboard == "T" or keyboard == "t"):
+        selected_link_text = driver.find_element_by_class_name('orange-d').text
+
+        if selected_link_text == "Personal Details":
+            print("No Data Found ==> Automatic Submitting entry\n")
             driver.find_element_by_id('_changeStatus6').click()
-        elif(keyboard == "F" or keyboard == "f"):
-            driver.find_element_by_id('_changeStatus2').click()
-        else:
-            keyboard = input('Entry Values: Enter T for True / F for False')
-            if (keyboard == "T" or keyboard == "t"):
+
+        # Only ask for t/f if entry finds livestock, poultry data
+        if selected_link_text != "Personal Details":
+            keyboard = input('Entry Values: Enter T for True / F for False  ')
+            if keyboard == "T" or keyboard == "t":
                 driver.find_element_by_id('_changeStatus6').click()
-            elif (keyboard == "F" or keyboard == "f"):
+            elif keyboard == "F" or keyboard == "f":
                 driver.find_element_by_id('_changeStatus2').click()
+            else:
+                keyboard = input('Entry Values: Enter T for True / F for False')
+                if keyboard == "T" or keyboard == "t":
+                    driver.find_element_by_id('_changeStatus6').click()
+                elif keyboard == "F" or keyboard == "f":
+                    driver.find_element_by_id('_changeStatus2').click()
 
         driver.find_element_by_id('submit').click()
-        time.sleep(2)
-        alert = driver.switch_to.alert.accept()
-
+        time.sleep(1.0)
+        alert_check()
         loop_iter += 1
         print("successfully entered entry No. : " + str(loop_iter) + " \n")
 
-except:
+except Exception as e:
+    print(e)
+    print("-"*40)
     print("Successfully entered entries:" + str(loop_iter) + "\n")
     print("Logging out from website")
     driver.get("http://livestockcensus.gov.in/logout.action")
     print("Closing Browser.....\n")
     driver.close()
-    print("Thank you for using SelBot -- Dev.Jeel \n For more Contact Dev.")
+    print("Thank you for using SelBot -- Developer Jeel \n For more Contact Dev.")
